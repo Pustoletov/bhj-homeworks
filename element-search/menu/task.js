1e7d0201b53d64c1c3db59f3b78b72d87c3c519f
Siun  Maksim <myrdym@rambler.ru>
2021-09-01 16:53:13 +0300

    fix menu and slider

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -5,12 +5,9 @@ let arrForlink = Array.from(menuLink);
 
 arrForlink.forEach(elem => elem.onclick = () => {
   if (elem.closest("li").querySelector("ul.menu_sub")) {
-    elem.closest("li").querySelector("ul.menu_sub").classList.add("menu_active");
+    elem.closest("li").querySelector("ul.menu_sub").classList.toggle("menu_active");
     return false;
   }
-  // if (elem.closest("li").querySelector("ul.menu_sub").classList.contains("menu_active")) {
-  //   elem.closest("li").querySelector("ul.menu_sub").classList.remove("menu_active");
-  // }
 }) 
 
 
--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -6,58 +6,19 @@ let arrforSliderItem = Array.from(sliderItem);
 
 let indxSlide = 0
 
-const activeslide = (i) => {
-  arrforSliderItem.forEach(elem  => elem.classList.remove("slider__item_active"))
-  arrforSliderItem[i].classList.add("slider__item_active")
-}
-
-const ArrowNext = () => {
-  if  (indxSlide === arrforSliderItem.length - 1) {
-    indxSlide = 0;
-    activeslide(indxSlide)
-  } else {
-    indxSlide++
-    activeslide(indxSlide)
-  }
-}
-
-const ArrowPrev = () => {
-  if  (indxSlide === 0) {
-    indxSlide === arrforSliderItem.length - 1;
-    activeslide(indxSlide)
-  } else {
-    indxSlide--
-    activeslide(indxSlide)
-  }
-}
 
 sliderArrowNext[0].onclick = () => {
-  ArrowNext();
+  indxSlide === arrforSliderItem.length - 1 ? indxSlide = 0 : indxSlide++ 
+  ((i)=>{
+    arrforSliderItem.forEach(elem  => elem.classList.remove("slider__item_active"))
+    arrforSliderItem[i].classList.add("slider__item_active")
+  })(indxSlide);
 }
 
 sliderArrowPrev[0].onclick = () => {
-  ArrowNext();
+  indxSlide === 0 ? indxSlide = arrforSliderItem.length - 1 : indxSlide--
+  ((i)=>{
+    arrforSliderItem.forEach(elem  => elem.classList.remove("slider__item_active"))
+    arrforSliderItem[i].classList.add("slider__item_active")
+  })(indxSlide);
 }
-
-
-
-
-
-
-// const sliderItemLength = arrforSliderItem.length;
-
-// let nSlide = 0
-
-// sliderArrowNext[0].onclick = () => {
-//   arrforSliderItem.forEach((elem, i) => {
-//     if (elem.classList.contains("slider__item_active")) {
-//       elem.classList.toggle("slider__item_active")
-//     } else {
-//       elem.classList.toggle("slider__item_active")
-//     }
-//   });
-// }
-
-
-// elem.classList.add("slider__item_active")
-
