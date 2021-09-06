4557a37d3512d9075aa2edf065ac6bdac48858a8
Siun  Maksim <myrdym@rambler.ru>
2021-09-07 02:24:13 +0300

    tab fix

--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -1,22 +1,17 @@
-const tab = document.querySelectorAll(".tab");
-const tabContent = document.querySelectorAll(".tab__content");
-
-arrForTab = Array.from(tab);
-arrForTabContent = Array.from(tabContent);
+arrForTab = Array.from(document.querySelectorAll(".tab"));
+arrForTabContent = Array.from(document.querySelectorAll(".tab__content"));
 
 
 arrForTab.forEach(elem1 => {
-  elem1.addEventListener("click", (e) => {
-    if (elem1.classList.contains("tab_active")) {
-      elem1.classList.remove("tab_active");
-    }
-    arrForTabContent.forEach(elem2 => {
-      if (elem2.classList.contains("tab__content_active")) {
-        elem2.classList.remove("tab__content_active")
+  elem1.addEventListener("click", () => {
+    document.querySelectorAll(".tab").forEach(elem2=>elem2.classList.remove("tab_active"))
+    arrForTabContent.forEach(elem3 => {
+      if (elem3.classList.contains("tab__content_active")) {
+        elem3.classList.remove("tab__content_active")
       }
-      if ( arrForTab.indexOf(elem1) === arrForTabContent.indexOf(elem2)) {
-        elem1.classList.toggle("tab_active");
-        elem2.classList.toggle("tab__content_active");
+      if ( arrForTab.indexOf(elem1) === arrForTabContent.indexOf(elem3)) {
+        elem1.classList.add("tab_active");
+        elem3.classList.add("tab__content_active");
       }
     })
   })
